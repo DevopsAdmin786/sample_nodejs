dd6cded70cf112ae0431ec5aaae759d3a46bfd11
root <root@ip-172-31-5-99.ap-south-1.compute.internal>
2023-07-28 10:53:17 +0000

    max value added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-exports.handler = async (event) => {
+exports.handler = async (event) =>
+  const val=100
   const response = {
     statusCode: 200,
     body: JSON.stringify("Hello from Lambda and Github!"),
+    body: `The MAX value is : ${max}`	  
   }
   return response
 }
