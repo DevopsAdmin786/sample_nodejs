566fd7285a2829978911a928e079a06f7d446222
DevopsAdmin786 <106879991+DevopsAdmin786@users.noreply.github.com>
2023-10-18 17:31:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda One and Github!"),
+    body: JSON.stringify("Hello from Lambda JI and Github!"),
   }
   return response
 }
